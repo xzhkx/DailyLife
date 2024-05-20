74943df0fc5b7388a09bcfd62385848eb88ed421
zhk <hatuvu7180@gmail.com>
2024-05-21 04:13:28 +0700

    Movement complete

--- Assets/InputAction/PlayerMovement.cs
+++ Assets/InputAction/PlayerMovement.cs
@@ -6,28 +6,40 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-    private float speed;
-    private Rigidbody playerRigidbody;
+    private float runSpeed, rotationSpeed;
+    [SerializeField]
+    private Animator animator;
+
+    private CharacterController characterController;
     private PlayerInputActions playerInputActions;
 
     private void Awake()
-    {
-        playerRigidbody = GetComponent<Rigidbody>();
+    { 
+        characterController = GetComponent<CharacterController>();
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
 
-        playerInputActions.Player.Movement.performed += MovementPerformed;
+        animator.SetBool("isMove", false);
     }
 
     private void FixedUpdate()
     {
         Vector2 input = playerInputActions.Player.Movement.ReadValue<Vector2>();
-        playerRigidbody.velocity = new Vector3(input.x, 0, input.y) * speed;
-    }
 
-    private void MovementPerformed(InputAction.CallbackContext context)
-    {
-        Vector2 input = context.ReadValue<Vector2>();
-        playerRigidbody.velocity = new Vector3(input.x, 0, input.y) * speed;
-    }    
+        Vector3 inputConvert = new Vector3(input.x, 0, input.y);
+
+        characterController.Move(inputConvert * runSpeed * Time.fixedDeltaTime);
+
+        if (input == Vector2.zero)
+        {
+            animator.SetBool("isMove", false);
+          
+        } else
+        {
+            animator.SetBool("isMove", true);
+            Quaternion toRotation = Quaternion.LookRotation(inputConvert, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation,
+                rotationSpeed);
+        }      
+    } 
 }
