c8119b3b1cdfe5bc9100b044c9cf45e3a154aa4e
zhk <hatuvu7180@gmail.com>
2024-05-22 21:20:12 +0700

    stairs + map setup

--- Assets/InputAction/PlayerMovement.cs
+++ Assets/InputAction/PlayerMovement.cs
@@ -10,36 +10,91 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private Animator animator;
 
-    private CharacterController characterController;
+    [Header("Stair Handler")]
+    [SerializeField]
+    private Transform stepRayLower, stepRayUpper;
+    [SerializeField]
+    private float stepHeight, stepSmooth;
+    
     private PlayerInputActions playerInputActions;
+    private Rigidbody playerRigidbody;
 
     private void Awake()
     { 
-        characterController = GetComponent<CharacterController>();
+        playerRigidbody = GetComponent<Rigidbody>();
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
 
         animator.SetBool("isMove", false);
+
+        stepRayUpper.localPosition = new Vector3(stepRayUpper.localPosition.x, stepHeight, stepRayUpper.localPosition.z);
     }
 
     private void FixedUpdate()
+    {
+        HandlerMovement();
+        StepClimb();
+    } 
+
+    private void StepClimb()
+    {
+        RaycastHit hitLower;
+        if (Physics.Raycast(stepRayLower.position, transform.TransformDirection(Vector3.forward),
+            out hitLower, 0.1f))            
+        {
+            Debug.Log("Hit");
+            RaycastHit hitUpper;
+            if(!Physics.Raycast(stepRayUpper.position, transform.TransformDirection(Vector3.forward),
+                out hitUpper, 0.2f))
+            {
+                playerRigidbody.position -= new Vector3(0, -stepSmooth, 0);
+            }
+        }
+
+        RaycastHit hitLower45;
+        if(Physics.Raycast(stepRayLower.position, transform.TransformDirection(1.5f, 0, 1), 
+            out hitLower45, 0.1f))
+        {
+            RaycastHit hitUpper45;
+            if(!Physics.Raycast(stepRayUpper.position, transform.TransformDirection(1.5f, 0, 1),
+                out hitUpper45, 0.2f))
+            {
+                playerRigidbody.position -= new Vector3(0, -stepSmooth, 0);
+            }
+        }
+
+        RaycastHit hitLowerMinus45;
+        if (Physics.Raycast(stepRayLower.position, transform.TransformDirection(1.5f, 0, 1),
+            out hitLowerMinus45, 0.1f))
+        {
+            RaycastHit hitUpperMinus45;
+            if (!Physics.Raycast(stepRayUpper.position, transform.TransformDirection(1.5f, 0, 1),
+                out hitUpperMinus45, 0.2f))
+            {
+                playerRigidbody.position -= new Vector3(0, -stepSmooth, 0);
+            }
+        }
+    }
+
+    private void HandlerMovement()
     {
         Vector2 input = playerInputActions.Player.Movement.ReadValue<Vector2>();
 
         Vector3 inputConvert = new Vector3(input.x, 0, input.y);
 
-        characterController.Move(inputConvert * runSpeed * Time.fixedDeltaTime);
-
         if (input == Vector2.zero)
         {
             animator.SetBool("isMove", false);
-          
-        } else
+            playerRigidbody.velocity = Vector3.zero;
+
+        }
+        else
         {
             animator.SetBool("isMove", true);
+            playerRigidbody.velocity = inputConvert * runSpeed;
             Quaternion toRotation = Quaternion.LookRotation(inputConvert, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation,
                 rotationSpeed);
-        }      
-    } 
+        }
+    }
 }
